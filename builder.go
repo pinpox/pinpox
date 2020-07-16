a42ce11b90562aad27a2d1d48279439cdcfaed58
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2020-07-17 00:37:13 +0200

    add builder

---
+++ builder.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/SlyMarbo/rss"
+	"os"
+	"strings"
+	"text/template"
+)
+
+func main() {
+	feed, err := rss.Fetch("https://pablo.tools/index.xml")
+	if err != nil {
+		// handle error.
+	}
+
+	// ... Some time later ...
+
+	// fmt.Println(feed.Nickname)
+	// fmt.Println(feed.Title)
+
+	posts := make(map[string][]*rss.Item)
+
+	for _, v := range feed.Items {
+		if strings.HasPrefix(v.Link, "https://pablo.tools/posts") {
+			cat := strings.Title(strings.Split(v.Link, "/")[4])
+			posts[cat] = append(posts[cat], v)
+		}
+	}
+
+	t, err := template.ParseFiles("template")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(os.Stdout, posts)
+	if err != nil {
+		panic(err)
+	}
+
+}
